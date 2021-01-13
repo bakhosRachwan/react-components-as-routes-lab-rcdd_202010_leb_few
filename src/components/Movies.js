e2e8242f66a101d0d2de8306a2f8f4432318cc7e
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-13 12:45:47 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,18 +5,18 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
-      {movies.map((movie, index) => (
-          <div key={index}>
+      {movies.map((movie, index) => {
+          return <div key={index}>
               <h3>Name: {movie.title}</h3>
               <p>Time: {movie.time}</p>
               <p>Genres:</p>
               <ul>
-                  {movie.genres.map((genre, index) => (
-                      <li key={index}>{genre}</li>
-                  ))}
+                  {movie.genres.map((genre, index) => {
+                      return <li key={index}>{genre}</li>
+                  })}
               </ul>
           </div>
-      ))}
+      })}
     </div>
   );
 };
