6a1677bdd05ac7f18407cf98185a351fa7eddc21
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-13 12:45:05 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,17 +5,17 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
-      {actors.map((actor, index) => (
-          <div key={index}>
+      {actors.map((actor, index) => {
+          return <div key={index}>
               <h3>Name: {actor.name}</h3>
               <p>Movies:</p>
               <ul>
-                  {actor.movies.map((movie, index) => (
-                      <li key={index}>{movie}</li>
-                  ))}
+                  {actor.movies.map((movie, index) => {
+                    return  <li key={index}>{movie}</li>
+                  })}
               </ul>
           </div>
-      ))}
+      })}
     </div>
   );
 };
